321d256f7d91b748a26c7346fddd6419fd717728
Praveena0308 <Praveenabolli03@gmail.com>
2024-09-19 19:32:54 +0200

    updated

--- main.tf
+++ main.tf
@@ -1,45 +1,32 @@
 provider "aws" {
-    region = "eu-central-1"
-  
+  region = "eu-central-1"
 }
-resource "aws_key_pair" "ssh-key" {
-  key_name = "my-server-key"
-  public_key = file("/home/praveena/.ssh/id_rsa.pub")
-  
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name: "${var.env_prefix}-vpc"
   }
-
-resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "allow ssh inbound traffic"
-
-    ingress {
-        protocol = "tcp"
-        from_port = 22
-        to_port = 22
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        protocol = "-1"
-        from_port = 0
-        to_port = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  
-}
-output "aws_instance" {
-    value = aws_instance.my-server.public_ip
-  
 }
 
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
+  vpc_id = aws_vpc.myapp-vpc.id
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+}
 
-resource "aws_instance" "my-server" {
-    ami = "ami-04f76ebf53292ef4d"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.ssh-key.key_name
+module "myapp-server" {
+  source = "./modules/ec2"
+  vpc_id = aws_vpc.myapp-vpc.id
+  my_ip = var.my_ip
+  env_prefix = var.env_prefix
+  image_name = var.image_name
+  public_key_location = var.public_key_location
+  instance_type = var.instance_type
+  subnet_id = module.myapp-subnet.subnet.id
+  avail_zone = var.avail_zone
 
-    security_groups =  [aws_security_group.allow_ssh.name]
-
-    tags = {
-        Name = "my-instance"
-    }
 }
